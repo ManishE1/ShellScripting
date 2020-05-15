2d38324cf0ec4a13ba0232453ff374017116e638
kodip <kodipaka.manish@gmail.com>
2020-05-15 20:29:35 +0100

    implementing variables

---
+++ 03-variable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+message=welcome to devops
+echo $message
+echo ${message}
+
+banana=20
+echo number of bananas=${banana}N
+
+Date=$(date +%F)
+
+echo "today date is $date"
+
+Add=$((1+2+3+4+5))
+
+echo "total sum is $add"
