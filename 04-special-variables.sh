0ab3e1f99da448fc99c93b853bef4099e8cd8b2e
kodip <kodipaka.manish@gmail.com>
2020-05-15 21:14:35 +0100

    taking value from user

--- 04-special-variables.sh
+++ 04-special-variables.sh
@@ -3,3 +3,12 @@
 read -p 'Enter something :' something
 
 echo -e "The entered value is $something"
+
+
+
+echo "Script Name = $0"
+echo "First Argument = $1"
+echo "Second Argument = $2 "
+echo "All Arguments = $*"
+echo "All Arguments = $@"
+echo "Number of Arguments = $#"
