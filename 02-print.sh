f966b37960c445c8e88eb085347fbef0c842e74d
kodip <kodipaka.manish@gmail.com>
2020-05-15 19:31:07 +0100

    Different types of printing statements

---
+++ 02-print.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo hello world
+
+echo -e "hello world\nHello Devops"
+echo -e "hello\tWorld"
+echo -e "hello world\n\n\nBye world"
+
+
+echo -e "\e[33mMessage on Yellow Color\e[0m"
+echo -e "No colored Content Message"
+echo -e "\e[43;31mMessage of Red on Yellow\e[0m"
+echo -e "No colored Content Message"
+
